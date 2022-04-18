e11534f867d6d52ab88ade5d2964ff819d0bb224
David Roman <d@vidroman.dev>
2022-04-19 00:45:52 +0200

    Improve test failure messaging

--- Tests/PeriodDurationTests/ISO8601Tests.swift
+++ Tests/PeriodDurationTests/ISO8601Tests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class ISO8601Tests: XCTestCase {
     func testPeriodDurationScenarios() throws {
         for s in scenarios {
-            XCTAssertEqual(PeriodDuration(iso8601: s.input), s.output.map(PeriodDuration.init))
+            XCTAssertEqual(PeriodDuration(iso8601: s.input), s.output.map(PeriodDuration.init), "input: \(s.input)")
             if let output = s.output, s.strictForType == PeriodDuration.self {
                 XCTAssertEqual(PeriodDuration(output).formatted(style: .iso8601), s.input)
             }
@@ -13,7 +13,7 @@ final class ISO8601Tests: XCTestCase {
 
     func testPeriodScenarios() throws {
         for s in scenarios {
-            XCTAssertEqual(Period(iso8601: s.input), s.output.map(Period.init))
+            XCTAssertEqual(Period(iso8601: s.input), s.output.map(Period.init), "input: \(s.input)")
             if let output = s.output, s.strictForType == Period.self {
                 XCTAssertEqual(Period(output).formatted(style: .iso8601), s.input)
             }
@@ -22,7 +22,7 @@ final class ISO8601Tests: XCTestCase {
 
     func testDurationScenarios() throws {
         for s in scenarios {
-            XCTAssertEqual(Duration(iso8601: s.input), s.output.map(Duration.init))
+            XCTAssertEqual(Duration(iso8601: s.input), s.output.map(Duration.init), "input: \(s.input)")
             if let output = s.output, s.strictForType == Duration.self {
                 XCTAssertEqual(Duration(output).formatted(style: .iso8601), s.input)
             }
