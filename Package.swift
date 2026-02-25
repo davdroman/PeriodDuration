09fda3091b77611377e6d29ceba7be9b36781296
David Roman <2538074+davdroman@users.noreply.github.com>
2026-02-25 16:23:41 +0000

    Enable `MemberImportVisibility` (#33)

--- Package.swift
+++ Package.swift
@@ -46,5 +46,6 @@ for target in package.targets {
 	target.swiftSettings? += [
 		.enableUpcomingFeature("ExistentialAny"),
 		.enableUpcomingFeature("InternalImportsByDefault"),
+		.enableUpcomingFeature("MemberImportVisibility"),
 	]
 }
